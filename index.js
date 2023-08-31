932af34fac1a343c166d68a202f31db6c56fa898
Oladipin <>
2023-08-31 20:24:51 +0000

    testing some codes and designs 2

---
+++ index.js
@@ -0,0 +1,7 @@
+let playerOutcome = document.getElementById("player-outcome");
+let computerOutcome = document.getElementById("player-outcome");
+let choiceBtn = document.getElementsByClassName("choice-btn");
+let resultOutcome = document.getElementById("result-outcome");
+let player;
+let computer;
+let reslut;
